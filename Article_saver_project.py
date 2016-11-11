a93e549fca16dfaf7369d3db7c73841ddf7b67b0
Eileen <ehays.schwantes@gmail.com>
2016-11-10 21:31:03 -0800

    initial commit

---
+++ Article_saver_project.py
@@ -0,0 +1,37 @@
+# Goodreads for collecting lists of articles
+# Process:
+    # 1. Enter an article and Reading Status into (Dictionary names)
+    #         a. "Read"
+    #         b. "Currently reading"
+    #         c. "To-read"
+    #             1. Category 1 (e.g., Energy)
+    #             2. Category 2 (e.g., Politics)
+    #             3. Category 3
+    # # 2. Glean information from article(s) (Dictionary keys and value lists)
+    #     a. Header
+    #     b. Author
+    #     c. Publication Date
+    #     d. Blurb/description
+    #     e. Category (added by user - e.g., Politics, Entertainment, Energy)
+    # 3. Allow user to categorize into Reading Status and move around as needed
+    # 4. Added data
+    #     a. Category - 2e (don't know what to call this yet) to itemize their list
+    #     b. Date Added
+    #     c. Rating
+    #     d. Comments on article
+    # 5. Recommender system -may be too much to handle...
+    # 6. Front-end interface
+        a. abilty to view all your dictionaries
+        b. ability to check off an article from "To-read" and turn it into "Read"; sends you to a page for Rating
+        c. ability to share with others (email, social media)
+        d. search for other articles with similar key words?? (Google checker)
+
+
+    Project Title: News collector
+    Major Features/Requirements:
+    Github link to documentation:
+    Github link to pseudocode:
+    Expected code features: list, dictionaries, modules, classes, external files, APIs, others
+    External modules:
+    External APIs: ability to use RSS feed, or APIs from different news sources...that seems limiting
+    Major challenges to tackle:
